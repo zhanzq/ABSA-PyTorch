274a44fc98e3de6f21abe58cec5373ddef6e9c2f
zhanzq <zhanzhiqiang09@126.com>
2021-11-19 09:56:30 +0800

    format code

--- layers/dynamic_rnn.py
+++ layers/dynamic_rnn.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-# file: dynamic_rnn.py
-# author: songyouwei <youwei0314@gmail.com>
-# Copyright (C) 2018. All Rights Reserved.
-
-
 import torch
 import torch.nn as nn
 import numpy as np
--- layers/squeeze_embedding.py
+++ layers/squeeze_embedding.py
@@ -1,12 +1,7 @@
-# -*- coding: utf-8 -*-
-# file: squeeze_embedding.py
-# author: songyouwei <youwei0314@gmail.com>
-# Copyright (C) 2018. All Rights Reserved.
-
-
 import torch
 import torch.nn as nn
 import numpy as np
+
 
 class SqueezeEmbedding(nn.Module):
     """
