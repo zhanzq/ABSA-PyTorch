63178870f2c0b0fba0d9072397e913a09e9e4cfc
zhanzq <zhanzhiqiang09@126.com>
2021-11-22 10:56:46 +0800

    format code

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 python train.py \
     --model_name bert_spc \
     --dataset xp \
-    --pretrained_bert_name "/Users/zhanzq/Downloads/models/bert-base-chinese"
+    --pretrained_bert_name /Users/zhanzq/Downloads/models/bert-base-chinese \
+	--best_model_path state_dict/bert_spc_xp.bin
